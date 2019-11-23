ed050f1350c5b6d70284d1e2913eae25724c237d
Ken Goad <goadke@g.cofc.edu>
2019-11-23 22:40:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagram)
+      
+    end
+  end
+
+end  
